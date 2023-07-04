5f6b8b8c2b62d157a3d0e18844c55da6420c3d1e
EmmilyOdhiambo <emmilysodhiambo@gmail.com>
2023-07-04 21:42:02 +0300

    React.js

---
+++ src/introduction/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+const Introduction = ()=>{
+    return(
+    
+        <div className="intro">
+        <hi>What I learnt in React</hi>
+        <p>It is a js library developed by Facebook</p>
+        <p>It has reuseable components</p>
+   
+        </div>
+        
+    )
+}
+export default Introduction;
---
+++ src/navigation/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './style.css'
+const Navigation = ()=>{
+    return(
+    
+        <div className="navbar">
+             <nav>
+        <ul>
+          <li><a href="">GAD<span id="pac">GET</span></a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">Today's Deal</a></li>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Contact us</a></li> 
+         
+      </ul>
+    </nav>
+        </div>
+        
+        
+        
+    )
+}
+export default Navigation;
